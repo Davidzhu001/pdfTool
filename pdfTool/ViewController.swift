ef790bd4b55c24914d869dfde295bb868780d429
Weicheng Zhu <weicheng.zhu@icloud.com>
2016-12-04 22:41:04 -0600

    Pdf playground

--- pdfTool/ViewController.swift
+++ pdfTool/ViewController.swift
@@ -8,20 +8,37 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+import Quartz
 
+class ViewController: NSViewController {
+    @IBOutlet weak var currentPageLabel: NSTextField!
+
+    @IBOutlet weak var inputValue: NSTextField!
+    @IBOutlet weak var pdfViewer: PDFView!
+    var pdfview = PDFDocument()
+    var globalUrl = PDFDocument();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        let path =  Bundle.main.path(forResource: "myPDF", ofType: "pdf")!
+        let url = NSURL(fileURLWithPath: path)
+        let pdf = PDFDocument(url: url as URL)
+        self.globalUrl = pdf!
+        print(pdf?.pageCount)
+        let pageTow = pdf?.page(at: 5)
 
+        self.pdfViewer.document = pdf
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
+    @IBAction func confirmButton(_ sender: Any) {
+        var inputVaule = self.inputValue.intValue
+        self.pdfViewer.go(to:  self.globalUrl.page(at: Int(inputVaule))! )
+    }
+    
 }
-
