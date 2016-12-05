071305f081e9c7d8afd7279f6c6172d4cb285a4d
Weicheng Zhu <weicheng.zhu@icloud.com>
2016-12-04 21:29:18 -0600

    Initial Commit

---
+++ pdfTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  pdfTool
+//
+//  Created by Weicheng Zhu on 12/4/16.
+//  Copyright © 2016 Weicheng Zhu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ pdfTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pdfTool
+//
+//  Created by Weicheng Zhu on 12/4/16.
+//  Copyright © 2016 Weicheng Zhu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
