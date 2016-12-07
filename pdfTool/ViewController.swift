3a543c9fe2f6a565461b75a08b9559d1077457dd
davidzhu001 <qluoluo@icloud.com>
2016-12-07 15:12:20 -0600

    sperate different classes for pdf

--- pdfTool/ViewController.swift
+++ pdfTool/ViewController.swift
@@ -13,51 +13,87 @@ import Quartz
 class ViewController: NSViewController {
     @IBOutlet weak var currentPageLabel: NSTextField!
 
-    @IBOutlet weak var inputValue: NSTextField!
-    @IBOutlet weak var pdfViewer: PDFView!
-    var globalPDF = PDFDocument();
-    var newPdf = PDFDocument();
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let path =  Bundle.main.path(forResource: "myPDF", ofType: "pdf")!
-        let url = NSURL(fileURLWithPath: path)
-        let pdf = PDFDocument(url: url as URL)
-        self.globalPDF = pdf!
-        self.pdfViewer.autoScales = true;
-        print(pdf?.pageCount)
-        
-        let pageTow = pdf?.page(at: 2)
-
-        self.pdfViewer.document = pdf
+    @IBAction func openFile(_ sender: Any) {
+       let file_url =  openFile()
+        print(file_url)
     }
     
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    @IBOutlet weak var inputValue: NSTextField!
+    @IBOutlet weak var originPdfView: PDFView!
+    @IBOutlet var previewPdfView: PDFView!
+
+    
+    struct PdfFiles {
+        var originVersion = PDFDocument();
+        var workingVersion = PDFDocument();
+        var previewVersion =  PDFDocument();
     }
+    
+    
+    var originPDF = PDFDocument();
+    var newPDF = PDFDocument();
     @IBAction func confirmButton(_ sender: Any) {
         let inputInt = Int(self.inputValue.intValue)
-        if inputInt < (self.pdfViewer.document?.pageCount)! {
-            self.pdfViewer.go(to:  self.globalPDF.page(at: inputInt)! )
-            newPdf.insert((self.pdfViewer.document?.page(at: inputInt))!, at: 0)
-            print(newPdf.pageCount)
-            
-            do {
-                try newPdf.write(to: getDocumentsDirectory(filename: "you guess"))
-            } catch {
-                print(error)
-            }
+        if inputInt < (self.originPdfView.document?.pageCount)! {
+            self.originPdfView.go(to:  self.originPDF.page(at: inputInt)! )
+            newPDF.insert((self.originPdfView.document?.page(at: inputInt))!, at: 0)
+            print(newPDF.pageCount)
         } else
         {
             currentPageLabel.stringValue = "Out of page number"
         }
     }
-    func getDocumentsDirectory(filename :String) -> URL {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let path =  Bundle.main.path(forResource: "myPDF", ofType: "pdf")!
+        let url = NSURL(fileURLWithPath: path)
+        let pdf = PDFDocument(url: url as URL)
+        self.originPDF = pdf!
+        self.originPdfView.autoScales = true;
+        
+        self.originPdfView.document = self.originPDF
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    func writeFile(filename :String, file :PDFDocument) {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let documentsDirectory = paths[0]
-        return documentsDirectory.appendingPathComponent(filename + ".pdf")
+        let newFilePath =  documentsDirectory.appendingPathComponent(filename + ".pdf")
+        file.write(to: newFilePath)
+
     }
+    
+    func openFile() -> String {
+        
+        let myFileDialog: NSOpenPanel = NSOpenPanel()
+        myFileDialog.allowedFileTypes = ["pdf"]
+        myFileDialog.runModal()
+        
+        // Get the path to the file chosen in the NSOpenPanel
+        let path = myFileDialog.url?.path
+        if path != nil {
+            print(path!)
+            let url = NSURL(fileURLWithPath: path!)
+            self.originPDF = PDFDocument(url: url as URL)!
+            self.newPDF = PDFDocument(url: url as URL)!
+            self.previewPdfView.document = PDFDocument(url: url as URL)!
+            self.originPdfView.document =  PDFDocument(url: url as URL)!
+            return path!
+
+        } else{
+            return "nothing selected"
+
+        }
+        
+    }
+    
+    
     
 }
